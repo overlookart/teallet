115ba0acb5998c301d7ee897a0d1004c25e24d9e
xzh <18334786772@163.com>
2023-12-26 09:27:50 +0800

    app 入口

---
+++ SUI/SUI/SUIApp.swift
@@ -0,0 +1,18 @@
+//
+//  SUIApp.swift
+//  SUI
+//
+//  Created by CaiGou on 2023/12/26.
+//  Copyright © 2023 YYDDL. All rights reserved.
+//
+
+import SwiftUI
+@main
+struct SUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
+
