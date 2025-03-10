5b09f6f6c787b26a53a1358bdbc86783975fa2b9
xzh <18334786772@163.com>
2025-03-10 17:34:25 +0800

    home list view

--- SUI/SUI/HomeView.swift
+++ SUI/SUI/HomeView.swift
@@ -8,24 +8,57 @@
 
 import SwiftUI
 
+struct DataModel: Identifiable {
+    var id: UUID = UUID()
+    var title: String = ""
+    var subtitle: String = ""
+}
+
+struct SectionDataModel: Identifiable {
+    var id: UUID = UUID()
+    var sectionName: String = ""
+    var items: [DataModel] = []
+}
+
 struct HomeView: View {
+    var datas: [SectionDataModel] = [
+        SectionDataModel(sectionName: "分组1", items: [
+            DataModel(title: "My Title", subtitle: "subtitle"),
+            DataModel(title: "标题", subtitle: "子标题")
+        ]),
+        SectionDataModel(sectionName: "小池--杨万里", items: [
+            DataModel(title: "泉眼无声惜细流", subtitle: "树阴照水爱晴柔"),
+            DataModel(title: "小荷才露尖尖角", subtitle: "早有蜻蜓立上头")
+        ])
+    ]
+    
+    
     var body: some View {
-        ZStack {
-            
-            VStack {
-                Spacer()
-                Circle()
-                    .foregroundColor(.purple)
-                Spacer()
+        List {
+            ForEach(datas) { section in
+                Section(header: Text(section.sectionName)) {
+                    ForEach(section.items) { item in
+                        HomeRowView(model: item)
+                    }
+                }
             }
         }
-        .background(Color.pink)
-            
     }
     
     init(){
         debugPrint("HomeView init")
-        
+    }
+}
+
+struct HomeRowView: View {
+    var model: DataModel
+    var body: some View {
+        VStack(alignment: .leading, spacing: 8){
+            Text(model.title)
+                .font(.headline)
+            Label(model.subtitle, systemImage: "exclamationmark.circle")
+                .font(.subheadline)
+        }
     }
 }
 
