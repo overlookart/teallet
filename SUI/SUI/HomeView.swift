c371372ae819678a9061b5f80a5fa5c0c0a9381d
xzh <18334786772@163.com>
2025-04-18 16:10:48 +0800

    列表视图

--- SUI/SUI/HomeView.swift
+++ SUI/SUI/HomeView.swift
@@ -24,7 +24,8 @@ struct HomeView: View {
     var datas: [SectionDataModel] = [
         SectionDataModel(sectionName: "法律法规", items: [
             DataModel(title: "行政处罚法", subtitle: "行政机关依法对违反行政管理秩序"),
-            DataModel(title: "化妆品管理条例", subtitle: "规范化妆品生产经营活动")
+            DataModel(title: "化妆品管理条例", subtitle: "规范化妆品生产经营活动"),
+            DataModel(title: "西班牙政府采购制度", subtitle: "西班牙政府采购制度")
         ]),
         SectionDataModel(sectionName: "小池--杨万里", items: [
             DataModel(title: "泉眼无声惜细流", subtitle: "树阴照水爱晴柔"),
@@ -44,14 +45,54 @@ struct HomeView: View {
                         .listRowBackground(Color.purple.opacity(0.1))
                         .listRowInsets(.init(top: 8, leading: 8, bottom: 8, trailing: 8))
                         .listItemTint(.green)
+                        .listRowSeparatorTint(.yellow, edges: .all)
+                        .listRowSeparator(.visible, edges: .all)
+                        .swipeActions(edge: .leading, allowsFullSwipe: true) {
+                            Button {
+                                debugPrint("Read Action")
+                            } label: {
+                                Label("Read", systemImage: "envelope.open")
+                            }
+                            .tint(.blue)
+                            Button { 
+                                debugPrint("Unread Action")
+                            } label: {
+                                Label("Unread", systemImage: "envelope.badge")
+                            }
+                        }
+                        .swipeActions(edge: .trailing, allowsFullSwipe: true) {
+                            Button {
+                                debugPrint("Delete Action")
+                            } label: {
+                                Label("Delete", systemImage: "trash")
+                            }
+                            .tint(.red)
+                            Button {
+                                debugPrint("Flag Action")
+                            } label: {
+                                Label("Flag", systemImage: "flag")
+                            }
+                            .tint(.orange)
+                        }
+                        //.badge("❤️")//设置徽章
+                        
                     }
                 }
+                .listSectionSeparatorTint(.cyan, edges: .all)
+                .listSectionSeparator(.hidden, edges: .top)
+                .headerProminence(.increased)
             }
-        }.listStyle(.plain)
+        }
+        .listStyle(.insetGrouped)
+        .listRowSpacing(0)
     }
     
     init(){
         debugPrint("HomeView init")
+    }
+    
+    private func tapGestureAction(){
+        debugPrint("点击事件")
     }
 }
 
